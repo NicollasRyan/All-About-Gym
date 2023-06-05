5464be13a4209002d345b4661cf8ed6a19a8cd76
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-06-05 15:07:05 -0300

    estilizando page Workout

--- src/pages/Workouts/index.tsx
+++ src/pages/Workouts/index.tsx
@@ -1,15 +1,50 @@
-import { Content, LinkTraninig, Title } from "./styled";
+import {
+  Box,
+  ContainerText,
+  Content,
+  Img1,
+  Img2,
+  LinkTraninig,
+  Title,
+} from "./styled";
+
+import Shoulders from "../../img/Shoulders.png";
+import backWorkout from "../../img/backWorkout.jpg";
+import bicepsTraining from "../../img/bicepsTraining.jpg";
+import Chest from "../../img/Chest.jpg";
+import legTraining from "../../img/legTraining.jpg";
+import triceps from "../../img/triceps.jpg";
 
 export function Workouts() {
   return (
     <Content>
       <Title>Veja mais sobre os tipos de treinos aqui:</Title>
-      <LinkTraninig to="/workouts/back">Costas</LinkTraninig>
-      <LinkTraninig to="/workouts/biceps">Biceps</LinkTraninig>
-      <LinkTraninig to="/workouts/chest">Peito</LinkTraninig>
-      <LinkTraninig to="/workouts/leg">Perna</LinkTraninig>
-      <LinkTraninig to="/workouts/shoulder">Ombros</LinkTraninig>
-      <LinkTraninig to="/workouts/triceps">Triceps</LinkTraninig>
+      <ContainerText>
+        <Box>
+          <LinkTraninig to="/workouts/back">Costas</LinkTraninig>
+          <Img1 src={backWorkout} />
+        </Box>
+        <Box>
+          <LinkTraninig to="/workouts/biceps">Biceps</LinkTraninig>
+          <Img2 src={bicepsTraining} />
+        </Box>
+        <Box>
+          <LinkTraninig to="/workouts/chest">Peito</LinkTraninig>
+          <Img2 src={Chest} />
+        </Box>
+        <Box>
+          <LinkTraninig to="/workouts/leg">Perna</LinkTraninig>
+          <Img1 src={legTraining} />
+        </Box>
+        <Box>
+          <LinkTraninig to="/workouts/shoulder">Ombros</LinkTraninig>
+          <Img1 src={Shoulders} />
+        </Box>
+        <Box>
+          <LinkTraninig to="/workouts/triceps">Triceps</LinkTraninig>
+          <Img2 src={triceps} />
+        </Box>
+      </ContainerText>
     </Content>
   );
 }
--- src/pages/Workouts/styled.ts
+++ src/pages/Workouts/styled.ts
@@ -3,13 +3,42 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Title = styled.h1`
+  color: #000;
+  max-width: 670px;
+  background-color: white;
   margin-bottom: 50px;
 `;
 
 export const Content = styled(Container)`
-  padding-top: 100px;
+  margin-top: 100px;
+`;
+
+export const ContainerText = styled(Container)`
+  background-color: #202124;
+  border-radius: 10px;
+
+  padding: 25px 0 25px 0;
 `;
 
 export const LinkTraninig = styled(Link)`
   margin-right: 20px;
+  text-decoration: none;
+`;
+
+export const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 45px;
+
+  align-items: center;
+`;
+
+export const Img1 = styled.img`
+  max-width: 550px;
+`;
+
+export const Img2 = styled.img`
+  max-width: 400px;
+  max-height: 450px;
 `;
